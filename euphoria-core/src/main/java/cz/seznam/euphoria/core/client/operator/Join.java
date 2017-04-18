fec8784e9ddff5a4e5cc04c74198be7ea0ed7e6d
Novotnik, Petr <petr.novotnik@firma.seznam.cz>
2017-04-18 18:48:13 +0200

    #92 Define Union#output#getNumPartitions() contract

--- euphoria-core/src/main/java/cz/seznam/euphoria/core/client/operator/Join.java
+++ euphoria-core/src/main/java/cz/seznam/euphoria/core/client/operator/Join.java
@@ -39,7 +39,10 @@ import java.util.Collection;
 import java.util.Objects;
 
 /**
- * Join two datasets by given key producing single new dataset.
+ * Join two datasets by given key producing single new dataset.<p>
+ *
+ * Unless explicitly specified, the number of output partitions will equal
+ * {@code max(left.numPartitions(), right.numPartitions()}.
  */
 @Recommended(
     reason =
