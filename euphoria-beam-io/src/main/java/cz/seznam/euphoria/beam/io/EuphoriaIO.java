46a5151ca1ee03813632098dae83dd1fff102007
marek.simunek <marek.simunek@firma.seznam.cz>
2018-10-24 13:36:06 +0200

    beam euphoriaIO reading part done

--- euphoria-beam-io/src/main/java/cz/seznam/euphoria/beam/io/EuphoriaIO.java
+++ euphoria-beam-io/src/main/java/cz/seznam/euphoria/beam/io/EuphoriaIO.java
@@ -1,112 +1,144 @@
+/*
+ * Copyright 2016-2018 Seznam.cz, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package cz.seznam.euphoria.beam.io;
 
-import static cz.seznam.euphoria.shadow.com.google.common.base.Preconditions.checkArgument;
-import static java.util.Objects.requireNonNull;
-
-import cz.seznam.euphoria.core.client.io.DataSink;
-import cz.seznam.euphoria.core.client.io.Writer;
-import java.io.IOException;
-import java.util.stream.StreamSupport;
-import org.apache.beam.sdk.transforms.Combine;
-import org.apache.beam.sdk.transforms.DoFn;
-import org.apache.beam.sdk.transforms.GroupByKey;
+import cz.seznam.euphoria.core.client.io.BoundedDataSource;
+import cz.seznam.euphoria.core.client.io.BoundedReader;
+import cz.seznam.euphoria.core.client.io.DataSource;
+import org.apache.beam.sdk.coders.Coder;
+import org.apache.beam.sdk.io.BoundedSource;
+import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.transforms.PTransform;
-import org.apache.beam.sdk.transforms.ParDo;
-import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PBegin;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.beam.sdk.values.PDone;
-import org.apache.beam.sdk.values.TypeDescriptors;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 public class EuphoriaIO {
 
-  public static <T> Write<T> write(DataSink<T> sink, int numPartitions) {
-    return new Write<>(sink, numPartitions);
+  public static <T> Read<T> read(DataSource<T> dataSource, Coder<T> outputCoder) {
+    return new Read<>(dataSource, outputCoder);
   }
 
-  /**
-   * Write transform for euphoria sinks.
-   *
-   * @param <T> type of data to write
-   */
-  public static class Write<T> extends PTransform<PCollection<T>, PDone> {
+  public static class Read<T> extends PTransform<PBegin, PCollection<T>> {
 
-    private final DataSink<T> sink;
-    private final int numPartitions;
+    private final DataSource<T> source;
+    private final Coder<T> outputCoder;
 
-    private Write(DataSink<T> sink, int numPartitions) {
-      this.sink = sink;
-      this.numPartitions = numPartitions;
+    Read(DataSource<T> source, Coder<T> outputCoder) {
+      this.source = source;
+      this.outputCoder = outputCoder;
     }
+
     @Override
-    public PDone expand(PCollection<T> input) {
-      checkArgument(
-          input.isBounded() == PCollection.IsBounded.BOUNDED,
-          "Write supports bounded PCollections only.");
+    public PCollection<T> expand(PBegin input) {
+      if (source.isBounded()) {
+        org.apache.beam.sdk.io.Read.Bounded<T> bounded =
+            org.apache.beam.sdk.io.Read.from(new BoundedSourceWrapper<>(source.asBounded(), this));
 
-      // right now this is probably the best place to initialize sink
-      sink.initialize();
-
-      input
-          .apply("extract-partition", ParDo.of(new PartitionFn<>(numPartitions)))
-          .setTypeDescriptor(
-              TypeDescriptors.kvs(TypeDescriptors.integers(), input.getTypeDescriptor()))
-          .apply("group-by-partition", GroupByKey.create())
-          .apply("write-result", ParDo.of(new WriteFn<>(sink)))
-          .apply(Combine.globally((it) ->
-              (int) StreamSupport.stream(it.spliterator(), false).count()))
-          .apply(ParDo.of(new DoFn<Integer, Void>() {
-
-            @SuppressWarnings("unused")
-            @ProcessElement
-            public void processElement() throws IOException {
-              sink.commit();
-            }
-          }));
-
-      return PDone.in(input.getPipeline());
-    }
-  }
-
-  private static class PartitionFn<T> extends DoFn<T, KV<Integer, T>> {
-
-    private final int numPartitions;
-
-    PartitionFn(int numPartitions) {
-      this.numPartitions = numPartitions;
-    }
-
-    @SuppressWarnings("unused")
-    @ProcessElement
-    public void processElement(
-        @Element T element, OutputReceiver<KV<Integer, T>> outputReceiver) {
-      final int partition = (element.hashCode() & Integer.MAX_VALUE) % numPartitions;
-      outputReceiver.output(KV.of(partition, element));
-    }
-  }
-
-  private static class WriteFn<T> extends DoFn<KV<Integer, Iterable<T>>, Integer> {
-
-    private final DataSink<T> sink;
-
-    WriteFn(DataSink<T> sink) {
-      this.sink = sink;
-    }
-
-    @SuppressWarnings("unused")
-    @ProcessElement
-    public void processElement(@Element KV<Integer, Iterable<T>> element) throws IOException {
-      final Writer<T> writer = sink.openWriter(requireNonNull(element.getKey()));
-      try {
-        for (T item : element.getValue()) {
-          writer.write(item);
-        }
-        writer.flush();
-        writer.commit();
-      } catch (IOException e) {
-        writer.rollback();
-      } finally {
-        writer.close();
+        return input.apply(bounded);
+      } else {
+        throw new UnsupportedOperationException("Unbounded is not supported for now.");
       }
     }
+
+    public Coder<T> getOutputCoder() {
+      return outputCoder;
+    }
+  }
+
+  public static class BoundedSourceWrapper<T> extends BoundedSource<T> {
+
+    private final BoundedDataSource<T> source;
+    private final Read<T> readInstance;
+
+    public BoundedSourceWrapper(BoundedDataSource<T> source, Read<T> readInstance) {
+      this.source = source;
+      this.readInstance = readInstance;
+    }
+
+    @Override
+    public List<? extends BoundedSource<T>> split(
+        long desiredBundleSizeBytes, PipelineOptions options) throws Exception {
+
+      List<BoundedDataSource<T>> splits = source.split(desiredBundleSizeBytes);
+      return splits.stream()
+          .map(source -> new BoundedSourceWrapper<>(source, readInstance))
+          .collect(Collectors.toList());
+    }
+
+    @Override
+    public long getEstimatedSizeBytes(PipelineOptions options) throws Exception {
+      return source.sizeEstimate();
+    }
+
+    @Override
+    public BoundedReader<T> createReader(PipelineOptions options) throws IOException {
+      cz.seznam.euphoria.core.client.io.BoundedReader<T> boundedReader = source.openReader();
+      return new BoundedReaderWrapper<>(boundedReader, this);
+    }
+
+    @Override
+    public Coder<T> getOutputCoder() {
+      return readInstance.getOutputCoder();
+    }
+  }
+
+  public static class BoundedReaderWrapper<T> extends BoundedSource.BoundedReader<T> {
+    private final BoundedReader<T> euphoriaReader;
+    private final BoundedSourceWrapper<T> boundedSourceWrapper;
+    private T currentElement;
+
+    public BoundedReaderWrapper(
+        BoundedReader<T> euphoriaReader, BoundedSourceWrapper<T> boundedSourceWrapper) {
+      this.euphoriaReader = euphoriaReader;
+      this.boundedSourceWrapper = boundedSourceWrapper;
+    }
+
+    @Override
+    public boolean start() throws IOException {
+      return advance();
+    }
+
+    @Override
+    public boolean advance() throws IOException {
+      boolean hasNext = euphoriaReader.hasNext();
+      if (hasNext) {
+        currentElement = euphoriaReader.next();
+      }
+      return hasNext;
+    }
+
+    @Override
+    public T getCurrent() throws NoSuchElementException {
+      return currentElement;
+    }
+
+    @Override
+    public void close() throws IOException {
+      euphoriaReader.close();
+    }
+
+    @Override
+    public BoundedSource<T> getCurrentSource() {
+      return boundedSourceWrapper;
+    }
+
   }
 }
---
+++ euphoria-beam-io/src/test/java/cz/seznam/euphoria/beam/io/EuphoriaIOTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2016-2018 Seznam.cz, a.s.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.seznam.euphoria.beam.io;
+
+import cz.seznam.euphoria.core.client.io.ListDataSource;
+import org.apache.beam.sdk.coders.StringUtf8Coder;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.values.PCollection;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class EuphoriaIOTest {
+
+  @Rule
+  public TestPipeline testPipeline = TestPipeline.create();
+
+  @Test
+  public void boundedReadTest() {
+
+    List<String> inputList = asList(
+        "one", "two", "three", "four", "five",
+        "one two three four four two two",
+        "one one one two two three");
+    ListDataSource<String> input = ListDataSource.bounded(inputList);
+
+    PCollection<String> output = testPipeline.apply(EuphoriaIO.read(input, StringUtf8Coder.of()));
+
+    PAssert.that(output).containsInAnyOrder(inputList);
+
+    testPipeline.run().waitUntilFinish();
+
+  }
+}
